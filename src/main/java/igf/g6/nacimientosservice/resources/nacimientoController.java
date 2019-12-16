0589e83f5a79739c7dc2b370a81d9df6be3fe907
dennisrmz <orellanadennis12@gmail.com>
2019-12-16 10:09:01 -0600

    Creando metodo update y getId

--- src/main/java/igf/g6/nacimientosservice/resources/nacimientoController.java
+++ src/main/java/igf/g6/nacimientosservice/resources/nacimientoController.java
@@ -3,12 +3,10 @@ package igf.g6.nacimientosservice.resources;
 
 import igf.g6.nacimientosservice.models.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class nacimientoController {
@@ -16,13 +14,38 @@ public class nacimientoController {
     @Autowired
     private nacimientoService nacimientoService;
 
+    //Ruta y Metodo Para Obtener Todos Los Nacimientos
     @RequestMapping("/nacimientos")
     public List<Persona> getAllNacimiento(){
         return nacimientoService.getAllNacimientos();
     }
 
+    //Ruta y Metodo Para Obtener Un Nacimientos
+    @RequestMapping("/nacimientos/{id}")
+    public Persona getNacimiento(@PathVariable Long id){
+        return nacimientoService.getNacimiento(id);
+    }
+
+    //Ruta y Metodo Para Obtener Agregar un Nacimientos
     @RequestMapping(method = RequestMethod.POST, value = "/nacimientos")
     public void addNacimiento(@RequestBody Persona nacimiento){
         nacimientoService.addNacimiento(nacimiento);
     }
+
+    //Ruta y Metodo Para Actualizar Un Nacimientos
+    @RequestMapping(method = RequestMethod.PUT, value = "/nacimientos/{id}")
+    public void updateNacimiento(@RequestBody Persona nacimiento, @PathVariable Long id){
+        nacimientoService.updateNacimiento(id, nacimiento);
+    }
+
+
+    /*
+    Ruta y Metodo Para Eliminar Un Nacimientos
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/nacimientos/{id}")
+    public void deleteNacimiento(@PathVariable Long id){
+        nacimientoService.deleteNacimiento(id);
+    }
+    */
+
 }
--- src/main/java/igf/g6/nacimientosservice/resources/nacimientoService.java
+++ src/main/java/igf/g6/nacimientosservice/resources/nacimientoService.java
@@ -1,5 +1,6 @@
 package igf.g6.nacimientosservice.resources;
 
+import igf.g6.nacimientosservice.interfaces.nacimientoRepository;
 import igf.g6.nacimientosservice.models.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,34 @@ import java.util.List;
 @Service
 public class nacimientoService {
     @Autowired
-    private igf.g6.nacimientosservice.interfaces.nacimientoRepository nacimientoRepository;
+    private nacimientoRepository nacimientoRepository;
 
+    //Mostrando Todos Los Nacimientos
     public List<Persona> getAllNacimientos(){
         List<Persona> nacimientos = new ArrayList<>();
         nacimientoRepository.findAll().forEach(nacimientos::add);
         return nacimientos;
     }
 
+    //Mostrando Un Nacimiento Especifico
+    public Persona getNacimiento(Long id) {
+        return nacimientoRepository.findById(id).get();
+    }
+
+    //Agregando Un Nacimiento
     public void addNacimiento(Persona nacimiento) {
         nacimientoRepository.save(nacimiento);
     }
+
+    //Actualizando un Nacimiento
+    public void updateNacimiento(Long id, Persona nacimiento){
+        nacimientoRepository.save(nacimiento);
+    }
+
+    /*
+    Eliminando Un Nacimiento
+    public void deleteNacimiento(Long id){
+    topicRepository.delete(id);
+    }
+     */
 }
